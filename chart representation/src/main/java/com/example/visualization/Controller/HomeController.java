048cd360ed088b31e9c7abcb57d07d69f7ab5493
AyshaRafeeq <70645662+AyshaRafeeq@users.noreply.github.com>
2020-10-01 16:23:41 +0530

    Update HomeController.java

--- chart representation/src/main/java/com/example/visualization/Controller/HomeController.java
+++ chart representation/src/main/java/com/example/visualization/Controller/HomeController.java
@@ -17,24 +17,48 @@ import com.google.gson.JsonObject;
 @Controller
 public class HomeController {
 	
-	
+	Integer id;
+	String name;
 	@Autowired
 	dataDAO dataDAO;
 	
-  @Autowired
+  	@Autowired
 	viewServiceImpl service;
 	
-	@RequestMapping("/")
+	@RequestMapping("/teacher")
 	public String showHome() {
 		//System.out.println("here");
-		return "index";
+		return "home";
 	}
 	
 	
+	@RequestMapping("/student")
+	public String studshowHome() {
+		return "student";
+	}
+	
+	@RequestMapping("/index")
+	public String index(Data data) {
+		id = data.getTestid();
+		return "index";
+	}
+	
+	@RequestMapping("/listStudents")
+	@GetMapping
+	public ModelAndView list() {
+		List<String> list =  service.getAllStudents();
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("view");
+		mv.addObject("name",list);
+		return mv;
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8082")
 	@RequestMapping("/linechartdata")
 	@ResponseBody
 	public String getDataFromDB(){
-		List<Data> dataList = service.getAllUsers();
+		List<Data> dataList = service.getStudentsbyid(id);
+		System.out.println(dataList);
 		JsonArray jsonArrayUname = new JsonArray();
 		JsonArray jsonArrayMarks = new JsonArray();
 		JsonObject jsonObject = new JsonObject();
@@ -43,6 +67,31 @@ public class HomeController {
 			jsonArrayMarks.add(data.getMarks());
 		});
 		jsonObject.add("uname", jsonArrayUname);
+		jsonObject.add("marks", jsonArrayMarks);
+		return jsonObject.toString();
+	}
+	
+	@RequestMapping("/individualreport")
+	public String report(String uname) {
+		//System.out.println(uname);
+		name= uname;
+		return "individual";
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8082")
+	@RequestMapping("/individualstuddata")
+	@ResponseBody
+	public String getData(){
+		List<Data> dataList = service.getStudentsbyname(name);
+		System.out.println(dataList);
+		JsonArray jsonArrayTestid = new JsonArray();
+		JsonArray jsonArrayMarks = new JsonArray();
+		JsonObject jsonObject = new JsonObject();
+		dataList.forEach(data->{
+			jsonArrayTestid.add(data.getTestid());
+			jsonArrayMarks.add(data.getMarks());
+		});
+		jsonObject.add("testid", jsonArrayTestid);
 		jsonObject.add("marks", jsonArrayMarks);
 		return jsonObject.toString();
 	}
