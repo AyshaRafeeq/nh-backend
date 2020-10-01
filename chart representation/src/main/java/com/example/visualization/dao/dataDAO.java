8913b343d31c71aefff3589c1841462ae98cf15d
AyshaRafeeq <70645662+AyshaRafeeq@users.noreply.github.com>
2020-10-01 16:25:14 +0530

    Update dataDAO.java

--- chart representation/src/main/java/com/example/visualization/dao/dataDAO.java
+++ chart representation/src/main/java/com/example/visualization/dao/dataDAO.java
@@ -8,5 +8,13 @@ import org.springframework.stereotype.Repository;
 import com.example.visualization.model.Data;
 
 @Repository
-public interface dataDAO extends JpaRepository<Data,String>{
+public interface dataDAO extends JpaRepository<Data,Integer>{
+  @Query(value="select id,testid,uname,marks from testmarks t where t.testid=?1",nativeQuery=true)
+	List<Data> findStudentsbyid(Integer id);
+	
+	@Query(value="select id,testid,uname,marks from testmarks t where t.uname=?1",nativeQuery=true)
+	List<Data> findStudentsbyname(String name);
+	
+	@Query(value="select distinct uname from testmarks",nativeQuery=true)
+	List<String> findAllStudents();
 }
